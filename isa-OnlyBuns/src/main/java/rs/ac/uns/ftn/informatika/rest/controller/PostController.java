b2bd00a7c61fdf5fe734cc4c967fa179adff8248
ra50ivana <ivanaglamocic666@gmail.com>
2024-11-12 04:15:29 +0100

    feat: Implemented creating post

--- isa-OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/controller/PostController.java
+++ isa-OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/controller/PostController.java
@@ -1,12 +1,16 @@
 package rs.ac.uns.ftn.informatika.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import rs.ac.uns.ftn.informatika.rest.domain.Post;
+import rs.ac.uns.ftn.informatika.rest.dto.PostDTO;
 import rs.ac.uns.ftn.informatika.rest.service.PostService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,4 +24,23 @@ public class PostController {
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
     }
+
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Post> createPost(@RequestPart("postDTO") PostDTO postDTO,
+                                           @RequestPart("imageFile") MultipartFile imageFile) {
+        // Provera da li je slika u formatu JPEG ili PNG
+        String contentType = imageFile.getContentType();
+        if (contentType == null || (!contentType.equals(MediaType.IMAGE_JPEG_VALUE) && !contentType.equals(MediaType.IMAGE_PNG_VALUE))) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        try {
+            Post createdPost = postService.createPost(postDTO, imageFile);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
 }
---
+++ isa-OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/dto/PostDTO.java
@@ -0,0 +1,71 @@
+package rs.ac.uns.ftn.informatika.rest.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PostDTO {
+    private Long id;
+    private String description;
+    private String imageUrl;
+    private int likes;
+    private List<CommentDTO> comments;
+    private Long userId;
+    private double longitude;
+    private double latitude;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
