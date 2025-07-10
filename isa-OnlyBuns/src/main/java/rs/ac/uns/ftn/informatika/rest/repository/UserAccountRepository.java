166f92e045e9bffa095aee6563a97e9942f35acb
ra50ivana <ivanaglamocic666@gmail.com>
2025-07-10 13:55:57 +0200

    feat: basic email notification for inactive users (no stats yet)

--- isa-OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/repository/UserAccountRepository.java
+++ isa-OnlyBuns/src/main/java/rs/ac/uns/ftn/informatika/rest/repository/UserAccountRepository.java
@@ -1,10 +1,11 @@
 package rs.ac.uns.ftn.informatika.rest.repository;
 
-import org.apache.catalina.User;
+//import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rs.ac.uns.ftn.informatika.rest.domain.UserAccount;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,10 @@ public interface UserAccountRepository extends JpaRepository<UserAccount, Long> 
 
     // Ostali custom metodi ako su potrebni
     Optional<UserAccount> findByUsername(String username);
+
+    List<UserAccount> findByLastActivityDateBeforeAndIsEnabledTrueAndLastNotificationSentDateBeforeOrLastNotificationSentDateIsNull(LocalDateTime activityDateTime, LocalDateTime notificationDateTime);
+
+    Optional<UserAccount> findByVerificationCode(String verificationCode);
 }
 
 
