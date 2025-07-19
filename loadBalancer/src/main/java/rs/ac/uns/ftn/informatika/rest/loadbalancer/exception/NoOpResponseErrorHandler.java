a607cf47711844513fa6e701bce770c81325bb59
stamenkovicx <natasastamenkovic99@gmail.com>
2025-07-19 23:31:50 +0200

    feat: implemented load balancer - made to be a separate application

---
+++ loadBalancer/src/main/java/rs/ac/uns/ftn/informatika/rest/loadbalancer/exception/NoOpResponseErrorHandler.java
@@ -0,0 +1,20 @@
+package rs.ac.uns.ftn.informatika.rest.loadbalancer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+public class NoOpResponseErrorHandler implements ResponseErrorHandler {
+
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        return false;
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        // Ne radi ništa
+    }
+}
---
+++ loadBalancer/src/test/java/rs/ac/uns/ftn/informatika/rest/loadbalancer/LoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.informatika.rest.loadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
